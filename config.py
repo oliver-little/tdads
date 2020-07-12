cab686eed601342323f9bd2c99c6a311daa732ec
gah <sc18gah@leeds.ac.uk>
2020-07-12 23:41:27 +0100

    Added SVM, Random Forest, Naive Bayes

--- config.py
+++ config.py
@@ -1,6 +1,30 @@
 # TDADS config
 
 modules = {
+    "naive_bayes": {
+        "fit": {
+
+        },
+        "predict": {
+
+        }
+    },
+    "svm": {
+        "fit": {
+
+        },
+        "predict": {
+
+        }
+    },
+    "random_forest": {
+        "fit": {
+
+        },
+        "predict": {
+
+        }
+    },
     "kmeans": {
         "fit": {
             "plot": False
