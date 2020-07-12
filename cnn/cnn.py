33754fcbcea36e6525e6cf42c7f25b605277fedc
gah <sc18gah@leeds.ac.uk>
2020-07-12 18:20:15 +0100

    Added makemodule script

--- cnn/cnn.py
+++ cnn/cnn.py
@@ -12,18 +12,18 @@ from tensorflow.keras.callbacks import LearningRateScheduler
 
 
 # read and transform data
-mnist = tf.keras.datasets.mnist
-(X_train, Y_train), (X_test, Y_test) = mnist.load_data()
-X_train = X_train.reshape((X_train.shape[0], 28, 28, 1))
-X_test = X_test.reshape((X_test.shape[0], 28, 28, 1))
-Y_train = to_categorical(Y_train)
-Y_test = to_categorical(Y_test)
-
+# mnist = tf.keras.datasets.mnist
+# (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
+# X_train = X_train.reshape((X_train.shape[0], 28, 28, 1))
+# X_test = X_test.reshape((X_test.shape[0], 28, 28, 1))
+# Y_train = to_categorical(Y_train)
+# Y_test = to_categorical(Y_test)
+#
 # use float32 and normalise between 0 and 1
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
-X_train = X_train / 255.0
-X_test = X_test / 255.0
+# X_train = X_train.astype('float32')
+# X_test = X_test.astype('float32')
+# X_train = X_train / 255.0
+# X_test = X_test / 255.0
 
 
 # plot the digits
@@ -35,90 +35,97 @@ X_test = X_test / 255.0
 # plt.subplots_adjust(wspace=-0.1, hspace=-0.1)
 # plt.show()
 
-
-# Generate extra images! Keras does this for us.
-datagen = ImageDataGenerator(
-    rotation_range=10,
-    zoom_range = 0.10,
-    width_shift_range = 0.1,
-    height_shift_range = 0.1
-)
-
-
-# plot the newly-generated data
-# X_train3 = X_train[9,].reshape((1,28,28,1))
-# Y_train3 = Y_train[9,].reshape((1,10))
-# plt.figure(figsize=(15,4.5))
-# for i in range(30):
-#     plt.subplot(3, 10, i+1)
-#     X_train2, Y_train2 = datagen.flow(X_train3,Y_train3).next()
-#     plt.imshow(X_train2[0].reshape((28,28)),cmap=plt.cm.binary)
-#     plt.axis('off')
-#     if i==9: X_train3 = X_train[11,].reshape((1,28,28,1))
-#     if i==19: X_train3 = X_train[18,].reshape((1,28,28,1))
-# plt.subplots_adjust(wspace=-0.1, hspace=-0.1)
-# plt.show()
+def fit(x_train, y_train):
+    x_train = x_train.reshape((x_train.shape[0], 28, 28, 1))
+    y_train = to_categorical(y_train)
+    # Generate extra images! Keras does this for us.
+    datagen = ImageDataGenerator(
+        rotation_range=10,
+        zoom_range = 0.10,
+        width_shift_range = 0.1,
+        height_shift_range = 0.1
+    )
 
 
-# Configure Neural Networks
-# This uses an adaptation of LeNet5's design
-nets = 15
-models = [0] * nets
-for j in range(nets):
-  models[j] = Sequential()
-  models[j].add(Conv2D(32, kernel_size = 3, activation = 'relu', input_shape = (28, 28, 1)))
-  models[j].add(BatchNormalization())
-  models[j].add(Conv2D(32, kernel_size = 3, activation = 'relu'))
-  models[j].add(BatchNormalization())
-  models[j].add(Conv2D(32, kernel_size = 5, strides = 2, padding = 'same', activation = 'relu'))
-  models[j].add(BatchNormalization())
-  models[j].add(Dropout(0.4))
-
-  models[j].add(Conv2D(64, kernel_size = 3, activation = 'relu'))
-  models[j].add(BatchNormalization())
-  models[j].add(Conv2D(64, kernel_size = 3, activation = 'relu'))
-  models[j].add(BatchNormalization())
-  models[j].add(Conv2D(64, kernel_size = 5, strides = 2, padding = 'same', activation = 'relu'))
-  models[j].add(BatchNormalization())
-  models[j].add(Dropout(0.4))
-
-  models[j].add(Conv2D(128, kernel_size = 4, activation='relu'))
-  models[j].add(BatchNormalization())
-  models[j].add(Flatten())
-  models[j].add(Dropout(0.4))
-  models[j].add(Dense(10, activation='softmax'))
-
-  models[j].compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
+    # plot the newly-generated data
+    # X_train3 = X_train[9,].reshape((1,28,28,1))
+    # Y_train3 = Y_train[9,].reshape((1,10))
+    # plt.figure(figsize=(15,4.5))
+    # for i in range(30):
+    #     plt.subplot(3, 10, i+1)
+    #     X_train2, Y_train2 = datagen.flow(X_train3,Y_train3).next()
+    #     plt.imshow(X_train2[0].reshape((28,28)),cmap=plt.cm.binary)
+    #     plt.axis('off')
+    #     if i==9: X_train3 = X_train[11,].reshape((1,28,28,1))
+    #     if i==19: X_train3 = X_train[18,].reshape((1,28,28,1))
+    # plt.subplots_adjust(wspace=-0.1, hspace=-0.1)
+    # plt.show()
 
 
-# Decrease learning rate each epoch - converge on a better solution
-# (and don't accidentally jump way down)
-annealer = LearningRateScheduler(lambda x: 1e-3 * 0.95 ** x)
+    # Configure Neural Networks
+    # This uses an adaptation of LeNet5's design
+    nets = 15
+    models = [0] * nets
+    for j in range(nets):
+        models[j] = Sequential()
+        models[j].add(Conv2D(32, kernel_size = 3, activation = 'relu', input_shape = (28, 28, 1)))
+        models[j].add(BatchNormalization())
+        models[j].add(Conv2D(32, kernel_size = 3, activation = 'relu'))
+        models[j].add(BatchNormalization())
+        models[j].add(Conv2D(32, kernel_size = 5, strides = 2, padding = 'same', activation = 'relu'))
+        models[j].add(BatchNormalization())
+        models[j].add(Dropout(0.4))
 
-# do the training - takes ~2.5hrs for me (NVidia Compute Capability ~6.1)
-history = [0] * nets
-epochs = 45
-for j in range(nets):
-  X_train2, X_val2, Y_train2, Y_val2 = train_test_split(X_train, Y_train, test_size = 0.1)
-  history[j] = models[j].fit_generator(datagen.flow(X_train2, Y_train2, batch_size=64),
-                                       epochs = epochs, steps_per_epoch = X_train2.shape[0]//64,
-                                       validation_data = (X_val2, Y_val2), callbacks = [annealer])
-  print("CNN {0:d}: Epochs={1:d}, Train Acc.={2:.5f}, Validation Acc.={3:.5f}".format(j+1, epochs, max(history[j].history['acc']),max(history[j].history['val_acc'])))
+        models[j].add(Conv2D(64, kernel_size = 3, activation = 'relu'))
+        models[j].add(BatchNormalization())
+        models[j].add(Conv2D(64, kernel_size = 3, activation = 'relu'))
+        models[j].add(BatchNormalization())
+        models[j].add(Conv2D(64, kernel_size = 5, strides = 2, padding = 'same', activation = 'relu'))
+        models[j].add(BatchNormalization())
+        models[j].add(Dropout(0.4))
+
+        models[j].add(Conv2D(128, kernel_size = 4, activation='relu'))
+        models[j].add(BatchNormalization())
+        models[j].add(Flatten())
+        models[j].add(Dropout(0.4))
+        models[j].add(Dense(10, activation='softmax'))
+
+        models[j].compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
 
 
-# predict for test data
-results = np.zeros((X_test.shape[0], 10))
-for j in range(nets):
-  results = results + models[j].predict(X_test)
-results = np.argmax(results, axis=1)
-results = pd.Series(results, name='Label')
+    # Decrease learning rate each epoch - converge on a better solution
+    # (and don't accidentally jump way down)
+    annealer = LearningRateScheduler(lambda x: 1e-3 * 0.95 ** x)
 
-# plot the results
-plt.figure(figsize=(15,6))
-for i in range(40):
-    plt.subplot(4, 10, i+1)
-    plt.imshow(X_test[i].reshape((28,28)),cmap=plt.cm.binary)
-    plt.title("predict=%d" % results[i],y=0.9)
-    plt.axis('off')
-plt.subplots_adjust(wspace=0.3, hspace=-0.1)
-plt.show()
+    # do the training - takes ~2.5hrs for me (NVidia Compute Capability ~6.1)
+    history = [0] * nets
+    epochs = 45
+    for j in range(nets):
+      X_train2, X_val2, Y_train2, Y_val2 = train_test_split(x_train, y_train, test_size = 0.1)
+      history[j] = models[j].fit_generator(datagen.flow(X_train2, Y_train2, batch_size=64),
+                                           epochs = epochs, steps_per_epoch = X_train2.shape[0]//64,
+                                           validation_data = (X_val2, Y_val2), callbacks = [annealer])
+      print("CNN {0:d}: Epochs={1:d}, Train Acc.={2:.5f}, Validation Acc.={3:.5f}".format(j+1, epochs, max(history[j].history['acc']),max(history[j].history['val_acc'])))
+    return history, models
+
+def predict(x_test, args):
+    history, models = args
+    x_test = x_test.reshape((x_test.shape[0], 28, 28, 1))
+
+    # predict for test data
+    results = np.zeros((x_test.shape[0], 10))
+    for j in range(nets):
+      results = results + models[j].predict(x_test)
+    results = np.argmax(results, axis=1)
+    results = pd.Series(results, name='Label')
+    return results
+
+    # plot the results
+    # plt.figure(figsize=(15,6))
+    # for i in range(40):
+    #     plt.subplot(4, 10, i+1)
+    #     plt.imshow(X_test[i].reshape((28,28)),cmap=plt.cm.binary)
+    #     plt.title("predict=%d" % results[i],y=0.9)
+    #     plt.axis('off')
+    # plt.subplots_adjust(wspace=0.3, hspace=-0.1)
+    # plt.show()
--- kmeans/kmeans.py
+++ kmeans/kmeans.py
@@ -3,12 +3,8 @@ import numpy as np
 from sklearn import cluster, metrics
 import matplotlib.pyplot as plt
 
-mnist = keras.datasets.mnist
-(train_x, train_y), (test_x, test_y) = mnist.load_data()
-
-# flatten image data using list comprehension
-train_x_flat = [[item for sublist in image for item in sublist] for image in train_x]
-test_x_flat = [[item for sublist in image for item in sublist] for image in test_x]
+# mnist = keras.datasets.mnist
+# (train_x, train_y), (test_x, test_y) = mnist.load_data()
 
 
 def infer_cluster_labels(kmeans, actual_labels):
@@ -70,38 +66,50 @@ def infer_data_labels(X_labels, cluster_labels):
 
     return predicted_labels
 
+def fit(x_train, train_y):
+    # flatten image data using list comprehension
+    train_x_flat = [[item for sublist in image for item in sublist] for image in x_train]
 
-# 1024 clusters (to avoid mislabelling)
-k = 1024
-kmeans = cluster.MiniBatchKMeans(n_clusters = k, init_size=k*3, random_state=0)
-kmeans.fit(train_x_flat)
+    # 1024 clusters (to avoid mislabelling)
+    k = 1024
+    kmeans = cluster.MiniBatchKMeans(n_clusters = k, init_size=k*3, random_state=0)
+    kmeans.fit(train_x_flat)
 
-# convert flattened centroid images back into full-size
-centroids = kmeans.cluster_centers_
-images = centroids.reshape(k, 28, 28)
+    # convert flattened centroid images back into full-size
+    centroids = kmeans.cluster_centers_
+    images = centroids.reshape(k, 28, 28)
 
-# calculate cluster labels
-cluster_labels = infer_cluster_labels(kmeans, train_y)
+    # calculate cluster labels
+    cluster_labels = infer_cluster_labels(kmeans, train_y)
 
-# find different numbered clusters and sort into numerical order
-images_to_show = []
-titles = []
-for i in range(10):
-    titles.append(i)
-    images_to_show.append(images[cluster_labels[i][0]])
+    # find different numbered clusters and sort into numerical order
+    images_to_show = []
+    titles = []
+    for i in range(10):
+        titles.append(i)
+        images_to_show.append(images[cluster_labels[i][0]])
 
-# plot (some) centroids
-fig, axs = plt.subplots(5, 2, figsize = (20, 20))
-plt.gray()
+    if 'plot' in kwargs:
+        # plot (some) centroids
+        fig, axs = plt.subplots(5, 2, figsize = (20, 20))
+        plt.gray()
 
-for i, ax in enumerate(axs.flat):
-    ax.set_title(titles[i])
-    ax.matshow(images_to_show[i])
-    ax.axis('off')
+        for i, ax in enumerate(axs.flat):
+            ax.set_title(titles[i])
+            ax.matshow(images_to_show[i])
+            ax.axis('off')
 
-plt.show()
+        plt.show()
 
-# calculate and print accuracy
-test_clusters = kmeans.predict(test_x_flat)
-predicted_labels = infer_data_labels(test_clusters, cluster_labels)
-print(f'Accuracy: {metrics.accuracy_score(test_y, predicted_labels)}')
+    return kmeans,
+
+def predict(test_x, args):
+    kmeans = args[0]
+    # flatten image data using list comprehension
+    test_x_flat = [[item for sublist in image for item in sublist] for image in test_x]
+
+    # calculate and print accuracy
+    test_clusters = kmeans.predict(test_x_flat)
+    predicted_labels = infer_data_labels(test_clusters, cluster_labels)
+    print(f'Accuracy: {metrics.accuracy_score(test_y, predicted_labels)}')
+    return predicted_labels
