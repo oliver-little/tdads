3957a2cb4e79130e9f967f831e0419a061d5a66f
lxz10 <laurenalie0@gmail.com>
2020-07-12 18:15:46 +0100

    added confusion matrix

--- feedforward.py
+++ feedforward.py
@@ -7,10 +7,10 @@ from tensorflow.python.keras.layers.core import *
 from tensorflow.python.keras.optimizers import *
 import tensorflow as tf
 from keras.utils import plot_model
-#This sklearn import statement below is causing this issue:
-#ImportError: DLL load failed: The specified module could not be found.
-#from sklearn.model_selection import GridSearchCV
-
+from keras import metrics
+import sklearn.metrics as skl
+import seaborn as sns
+#Hyperparamter tuning can be imported from optimisation.py if necessary
 
 '''A simple feed forward neural network with two hidden layers.'''
 
@@ -60,61 +60,6 @@ def one_hot(y):
 
     return out
 
-#Fine tuning epochs number
-def optimise_epochs():
-    epochs = [10, 15, 20]
-    param_grid = dict(epochs=epochs)
-    grid = GridSearchCV(estimator=model, param_grid=param_grid, cv=3)
-    grid_result = grid.fit(x_train, y_train)
-    # Summarise results
-    print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
-    means = grid_result.cv_results_['mean_test_score']
-    stds = grid_result.cv_results_['std_test_score']
-    params = grid_result.cv_results_['params']
-    for mean, stdev, param in zip(means, stds, params):
-        print("%f (%f) with: %r" % (mean, stdev, param))
-
-#Fine tuning optimiser
-def optimise_optimizer():
-    optimizer = ['SGD', 'RMSprop', 'Adagrad', 'Adadelta', 'Adam', 'Adamax', 'Nadam', 'Ftrl']
-    param_grid = dict(optimizer=optimizer)
-    grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=1, cv=3)
-    grid_result = grid.fit(x_train, y_train)
-    # Summarise results
-    print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
-    means = grid_result.cv_results_['mean_test_score']
-    stds = grid_result.cv_results_['std_test_score']
-    params = grid_result.cv_results_['params']
-    for mean, stdev, param in zip(means, stds, params):
-        print("%f (%f) with: %r" % (mean, stdev, param))
-
-#Fine tuning learning rate
-def optimise_lrate():
-    learn_rate = [0.001, 0.01, 0.1, 0.2, 0.3]
-    param_grid = dict(learn_rate=learn_rate)
-    grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=1, cv=3)
-    grid_result = grid.fit(x_train, y_train)
-    # Summarise results
-    print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
-    means = grid_result.cv_results_['mean_test_score']
-    stds = grid_result.cv_results_['std_test_score']
-    params = grid_result.cv_results_['params']
-    for mean, stdev, param in zip(means, stds, params):
-        print("%f (%f) with: %r" % (mean, stdev, param))
-
-#Fine tuning activation function
-def optimise_activation():
-    activation = ['softmax', 'softplus', 'softsign', 'relu', 'tanh', 'sigmoid', 'hard_sigmoid', 'linear']
-    param_grid = dict(activation=activation)
-    grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=1, cv=3)
-    grid_result = grid.fit(x_train, y_train)
-    # Summarise results
-    print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
-    means = grid_result.cv_results_['mean_test_score']
-    stds = grid_result.cv_results_['std_test_score']
-    params = grid_result.cv_results_['params']
-    for mean, stdev, param in zip(means, stds, params):
-        print("%f (%f) with: %r" % (mean, stdev, param))
 
 if __name__ == '__main__':
     x_train, x_test, y_train, y_test = get_data()
@@ -178,6 +123,20 @@ if __name__ == '__main__':
     print("Predictions for 10 samples")
     predictions = model.predict(x_test[:10])  
     y_new = model.predict_classes(x_test[:10])
+    y_pred = model.predict(x_test)
+
+    #Generate confusion matrix
+
+    # rounded_predictions = model.predict_classes(x_test, batch_size=128, verbose=0
+    Y_pred = np.argmax(y_pred, 1)
+    Y_test = np.argmax(y_test, 1)
+
+    matrix = skl.confusion_matrix(Y_test, Y_pred)
+
+    sns.heatmap(matrix.T, square=True, annot=True, cbar=False, cmap=plt.cm.Blues)
+    plt.xlabel('Predicted Values')
+    plt.ylabel('True Values')
+    plt.show()
 
     print("Visualising predictions")
     num_row = 2
