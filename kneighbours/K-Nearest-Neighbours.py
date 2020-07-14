97bc36b43791f434589bb9b0df09afcaf7c192fb
KotiJaddu <root@DESKTOP-SFTOBLQ.localdomain>
2020-07-14 13:55:36 +0100

    added functions to both models

--- kneighbours/K-Nearest-Neighbours.py
+++ kneighbours/K-Nearest-Neighbours.py
@@ -1,66 +1,89 @@
 '''
-# Recognising MNIST digits using K Nearest Neighbours without parallel programming 
+# Recognising MNIST digits using K Nearest Neighbours with parallel programming 
 # 
 # Author: Koti Jaddu
 '''
 
 # Import of libraries
-import numpy as np
+import numpy as np 
 import pandas as pd
 import mnist
 import time
+import ray
+import math
 
-K_VALUE = 10 # K hyperparameter
+# THREADS_N = 16 # Constant holding the number of threads for parallel processes
 
-# Load the training and testing images along with their respective labels
-train_images = mnist.train_images()
-train_labels = mnist.train_labels()
-test_images = mnist.test_images()
-test_labels = mnist.test_labels()
+# K_VALUE = 10 # K hyperparameter
 
-# Flatten each image from a 28x28 array to a 784 size vector
-train_images = train_images.reshape((-1, 784))
-test_images = test_images.reshape((-1, 784))
+def fit(x_train, y_train) :
+    # Flatten each image from a 28x28 array to a 784 size vector
+    x_train = x_train.reshape((-1, 784))
+    return x_train, y_train
 
-# Routine that will predict the digit when given a 784 vector image
-def predict_digit(test_image):
+
+# Initialise parameters for parallel processing
+ray.init()
+
+# Routine that will be compatible with parallel processing which will predict the digit when given a 784 vector image
+@ray.remote
+def predict_digit(test_image, args, k_value):
     df = pd.DataFrame()
+    x_train, y_train = args
+    # Loop through the entire training images set
+    for i in range(len(x_train)):
 
-	# Loop through the entire training images set
-    for i in range(len(train_images)):
-
-        # Calculate the Euclidean distance: sum of the errors for each index in the 784 vector test image with each training images
-        euclidean_distance = np.sum(np.square(np.subtract(train_images[i], test_image, dtype=np.float64), dtype=np.float64))
+        # Calculate the Euclidean distance: sum of the errors for each index in the 784 vector test image with each training image
+        euclidean_distance = np.sum(np.square(np.subtract(x_train[i], test_image, dtype=np.float64), dtype=np.float64))
 
         # Add this value along with the training label into a new row of the DataFrame df
-        row = pd.Series([train_labels[i], euclidean_distance])
+        row = pd.Series([y_train[i], euclidean_distance])
         row_df = pd.DataFrame([row])
         df = pd.concat([row_df, df], ignore_index=True)
 
-    # Rename the column names of the DataFrame df and sort the rows according to the euclidean_distance column in ascending order
-    df.columns = ['target', 'euclidean_distance']
+    # Rename the column names of the DataFrame df and sort the rows according to the euclidean_distance	column in ascending order
+    df.columns = ['training_labels', 'euclidean_distance']
     df.sort_values(['euclidean_distance'], inplace=True)
 
-    # Return the mode average of the training labels that are in the top K_VALUE rows
-    return df.head(K_VALUE).mode()['target'].iloc[0]
+    # Return the mode average of the training labels that are in the top k_value rows
+    return int(df.head(k_value).mode()['training_labels'].iloc[0])
 
-# Initialise variables that keep track of the total correct predictions and the time it takes to predict them
-total_correct = 0
-start = time.time()
 
-# Loop through each test image to be predicted
-for i in range(len(test_images)):
 
-    # Check the result to see if the prediction was correct and adjust the total_correct variable
-    if predict_digit(test_images[i]) == test_labels[i]:
-        total_correct += 1
+def predict(x_test, args, k_value = 10, threads_n = 8):
+    output = []
+    images_completed = 0
 
-    partial_end = time.time()
+    # Flatten each image from a 28x28 array to a 784 size vector
+    x_test = x_test.reshape((-1, 784))
 
-    # Print an update that shows the number of images predicted in batches of THREADS_N, the time it took, the accuracy so far and the estimated time to complete
-    print(f"{i + 1} image(s) predicted in {partial_end-start} seconds - Accuracy: {total_correct / (i + 1)} - ETA: {((partial_end-start)/(3600 * (i + 1))) * (len(test_images) - (i + 1))} hours")
+    # Loop through each test image to be predicted and split the tasks into threads_n threads for parallel processing
+    for i in range(len(x_test)//(threads_n)):
+        result_ids = []
 
-print()
+        # Create threads_n number of concurrent processes
+        for j in range(threads_n):
+            if threads_n * i + j < len(x_test):
+                result_ids.append(predict_digit.remote(x_test[threads_n * i + j], args, k_value))
+                images_completed += 1
 
-# Print the final statement that includes the total number of images predicted, the time it took and the final accuracy
-print(f"All {len(test_images)} images have been predicted in {(partial_end-start)/60} minutes with accuracy: {total_correct/len(test_images)}")
+        # Acquire the results
+        results = ray.get(result_ids)
+
+        # Add the results to what will be returned
+        output.extend(results)
+
+        # Print an update that shows the number of images predicted in batches of threads_n
+        print(f"{images_completed} images predicted out of {len(x_test)}")
+
+    return np.array(output)
+
+# Load the training and testing images along with their respective labels
+# train_images = mnist.train_images()
+# train_labels = mnist.train_labels()
+# test_images = mnist.test_images()
+# test_labels = mnist.test_labels()
+
+# Predict the first test image
+# print(predict(test_images, fit(train_images, train_labels))[0])
+# print(test_labels[0])
