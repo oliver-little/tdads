19337e874db875f20e81d7af9b1256248d8535cd
Rory Tallon <sc19rt@leeds.ac.uk>
2020-08-12 15:21:55 +0100

    Cleaned up the code

--- task_2/one_hot_encoder.py
+++ task_2/one_hot_encoder.py
@@ -1,5 +1,4 @@
 #one hot encoder
-
 class Encoder:
 
     def __init__(self, file):
@@ -14,23 +13,18 @@ class Encoder:
     def find_max_tweet(self):
         current_max = 0
         for row in self.tweets.text:
-            words = row.split(' ')
-
+            words = row.split(' '
             if len(words) > current_max:
                 current_max = len(words)
-
         self.max_tweet_length = current_max
 
-
     def dict_contains(self, word):
-
         for i in self.word_dictionary:
             if self.word_dictionary[i] == word:
                 return False
         return True
 
     def create_word_dictionary(self):
-        #key should be the number assigned to it
         for row in self.tweets.text:
             words = row.split(' ')
             for word in words:
@@ -40,26 +34,10 @@ class Encoder:
                     self.dict_key += 1
 
     def find_key(self, word):
-
         for i in self.word_dictionary:
             if self.word_dictionary[i] == word:
                 return i
         return -1
-
-
-    def convert_tweet(self, tweet):
-
-        tweet_vector = []
-        index = 0
-        for x in range(self.max_tweet_length):
-            tweet_vector.append(0)
-
-        words = tweet.split(' ')
-        for word in words:
-            tweet_vector[index] = self.find_key(word)
-            index += 1
-
-        return tweet_vector
 
     def create_tweet_vector(self, tweet):
         tweet_vector = []
